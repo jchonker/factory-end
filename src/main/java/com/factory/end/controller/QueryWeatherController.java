e6f555ac09b8305dd2e667d00f92fe71a88e16df
jchonker <jchonker@163.com>
2020-10-14 10:05:50 +0800

    自动获取访问者所在城市,并根据城市获取天气信息

--- src/main/java/com/factory/end/controller/QueryWeatherController.java
+++ src/main/java/com/factory/end/controller/QueryWeatherController.java
@@ -1,5 +1,7 @@
 package com.factory.end.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.factory.end.util.IpUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -29,19 +31,40 @@ public class QueryWeatherController {
     @Autowired
     RestTemplate restTemplate;
 
-    @RequestMapping(value = "/{city}", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     @ApiOperation("指定城市获取添加天气")
-    public String extern(HttpServletRequest request, @PathVariable("city") String city){
-        String ipAddr = IpUtil.getIpAddr(request);
-        logger.info("ip地址:"+ipAddr);
+    public String extern(HttpServletRequest request){
+//        String ipAddr = IpUtil.getIpAddr(request);
+//        logger.info("ip地址:"+ipAddr);
 
-        String apiURL = "http://wthrcdn.etouch.cn/weather_mini?city=" + city;
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiURL, String.class);
+        //获取访问者的城市信息
+        String citySN = "http://pv.sohu.com/cityjson";
+        ResponseEntity<String> cityInfoEntity = restTemplate.getForEntity(citySN, String.class);
+        if(200==cityInfoEntity.getStatusCodeValue()){
+            String cityStr = cityInfoEntity.getBody();
+            logger.info(cityStr);
+            //获取到的字符串为var returnCitySN = {...}形式,截取括号中的数据
+            int start = cityStr.indexOf('{');
+            int end = cityStr.indexOf('}');
+            String cityInfo = cityStr.substring(start, end+1);
+            logger.info("cityInfo:"+cityInfo);
+            JSONObject jsonObject = JSON.parseObject(cityInfo);
+            String cname = jsonObject.get("cname").toString();
+            logger.info("cname:"+cname);
+            //获取省后面的低级市级城市名
+            int index = cname.indexOf("省");
+            String prefectureLevel = cname.substring(index+1);
+            logger.info("地级市:"+prefectureLevel);
 
-        if(200==responseEntity.getStatusCodeValue()){
-            return responseEntity.getBody();
-        }else{
-            return "error with code : " + responseEntity.getStatusCodeValue();
+            //根据地级市获取城市所在的天气信息
+            String apiURL = "http://wthrcdn.etouch.cn/weather_mini?city=" + prefectureLevel;
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(apiURL, String.class);
+            if(200==responseEntity.getStatusCodeValue()){
+                return responseEntity.getBody();
+            }else{
+                return "error with code : " + responseEntity.getStatusCodeValue();
+            }
         }
+        return "error with code : " + cityInfoEntity.getStatusCodeValue();
     }
 }
